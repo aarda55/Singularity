ee14aac6026320e441c08a2c228f60f204e4c6e3
bb129 <79632956+aarda55@users.noreply.github.com>
2023-07-29 23:57:59 +0200

    Delete engine/activation function directory

--- engine/activation function/Sigmoid.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def Sigmoid(InZ):
-    return 1 / 1 + np.exp(-1*(InZ))
--- engine/activation function/tanh.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def tanh(InZ):
-    return (np.exp(InZ) - np.exp(-1*InZ))/(np.exp(InZ) + np.exp(-1*InZ))
