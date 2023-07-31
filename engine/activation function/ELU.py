eed9bf92eee55ad753aead9d5340736554b9fff0
bb129 <79632956+aarda55@users.noreply.github.com>
2023-07-31 17:45:51 +0200

    Delete engine directory

--- engine/activation function/ELU.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def ELU(InZ):
-    if InZ > 0:
-        return InZ
-    else:
-        return (np.exp(InZ)-1)
--- engine/activation function/Swish-1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import Sigmoid
-
-
-def Swish1(InZ):
-    return (InZ*Sigmoid(InZ))
