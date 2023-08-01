55b56c4de3e16e51becef573d0bfd76f9cfeaccd
bb129 <79632956+aarda55@users.noreply.github.com>
2023-08-01 22:54:18 +0200

    Delete engine directory

--- engine/core/input/Uni-Input_Layer.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def Input_layer(Input_data):
-    if Input_data == np.array or isinstance(Input_data, list) == True:
-        Input_data = np.array(Input_data)
-        return Input_data
-    else:
-        print("Singularity: Object being passed has to either be a python list or a numpy array!")
