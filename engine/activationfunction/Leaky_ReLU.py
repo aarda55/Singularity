6a33e60615f9a9b80693272a4ee446d5379a5d1e
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-08-07 23:14:47 +0200

    Delete engine directory

--- engine/activationfunction/Leaky_ReLU.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def Leaky_ReLu(Inp):
-    return np.maximum(0.01*Inp, Inp)
-
-def derivLeaky_ReLu(Inp):
-    return np.where(Inp>0, 1, 0.01) 
--- engine/activationfunction/__init__.py
+++
@@ -1,1 +0,0 @@
-from .ReLU import ReLU
