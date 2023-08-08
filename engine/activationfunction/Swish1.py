be85227edb47e0ce2fbbf640f417b803c655a161
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-08-08 21:55:40 +0200

    Delete engine directory

--- engine/activationfunction/Swish1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from Sigmoid import Sigmoid
-
-
-def Swish1(Inp):
-    return (Inp*Sigmoid(Inp))
-
-def derivSwish1(Inp):
-    return Sigmoid(Inp) * (1 + Inp * (1 - Sigmoid(Inp)))
--- engine/activationfunction/tanh.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def tanh(Inp):
-    return (np.exp(Inp) - np.exp(-1*Inp))/(np.exp(Inp) + np.exp(-1*Inp))
-
-def derivtanh(Inp):
-    return 1-((tanh(Inp)) ** 2)
