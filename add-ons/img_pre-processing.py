df03be935c0fee155fb564d2204c507c46b402b3
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-09-08 12:16:53 +0200

    Add files via upload

---
+++ add-ons/img_pre-processing.py
@@ -0,0 +1,37 @@
+import os
+
+import cv2
+import numpy as np
+
+
+def img_preprocessing(DATADIR, CATEGORIES, IMG_SIZE):
+    training_data = []
+
+    #formats data
+    def create_training_data():
+        for category in CATEGORIES:
+            path = os.path.join(DATADIR, category)
+            class_num = CATEGORIES.index(category)
+            for img in os.listdir(path):
+                try:
+                    img_array = cv2.imread(os.path.join(path,img))
+                    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                    training_data.append([new_array, class_num])
+                except Exception as e:
+                    pass
+
+    #creates data as arrays
+    create_training_data()
+    random.shuffle(training_data)
+    X = []
+    y = []
+
+    for features, label in training_data:
+        X.append(features)
+        y.append(label)
+    
+    X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+    X = X/255.0
+
+    return X, y
