f5626129cd7876c1fca8ce774df814ddb24e6b01
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-10-02 21:17:12 +0200

    Add files via upload

---
+++ engine/core/layers/MultiModel.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pickle
+import Model
+
+class MultiModel:
+    def __init__(self, models):
+        self.models = models  # List of pre-trained models
+
+    def predict_multi(self, X):
+        outputs = []
+
+        for model in self.models:
+            output = model.predict(X)
+            outputs.append(output)
+
+        return outputs
+    
+    def save_multi(self, paths):
+        for i, model in enumerate(self.models):
+            model.savem(paths[i])
+
+    @classmethod
+    def load_multi(cls, paths):
+        models = []
+
+        for path in paths:
+            model = Model.loadm(path) 
+            models.append(model)
+
+        return cls(models)
