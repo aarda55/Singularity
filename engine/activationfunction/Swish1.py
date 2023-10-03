4ef7b41438dc38d646479f196dfa46f4fa88b8be
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-10-03 12:36:24 +0200

    Delete engine directory

--- engine/activationfunction/Swish1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from Sigmoid import Sigmoid
-
-
-def Swish1(Inp, derivative=False):
-    if derivative:
-        return Sigmoid(Inp) * (1 + Inp * (1 - Sigmoid(Inp)))
-    return (Inp*Sigmoid(Inp))
--- engine/core/layers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .Model import *
-from .MultiModel import *
